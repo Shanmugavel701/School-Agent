a8e1d39a8ae4fbd8dfe8715cc91273f72eb883e4
Shanmugavel701 <smveditznkt@gmail.com>
2025-11-21 12:26:51 +0530

    Fix Render deployment: disable debug mode and optimize gunicorn

--- app.py
+++ app.py
@@ -234,4 +234,4 @@ def api_pdf():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
